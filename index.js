dac1c55b37e431447db4f27fa2dc9b4661d78ed0
bharatigupta2405 <bharatigupta2405@gmail.com>
2023-07-24 15:47:36 +0530

    Learnings

---
+++ index.js
@@ -0,0 +1,20 @@
+        // <<<------------- How to print something using Js ------------------------->>>>
+        // console.log('Hello World');//to print anything on console
+        // alert('Hi there');// to show an alert
+        // document.write(' This is document write');// to write something on browser but we should avoid this for some reason
+
+        // <--------------------------Console API(Application programming interface)------------------------------------------->>
+        // console.log('Hello World',4+6,"HI");//to print anything on console
+        // console.warn('this is a warning in console')// to show a warning in console not in browser
+        // console.error('to show error in console not in browser')
+        // console.clear()//to clear console
+        // console.assert(4==6)// it will check the condition and return accordingly for ex here O/P is index.html:46  Assertion failed: console.assert
+        // console.assert(4==(6-2))// it's correct so no Output assertion is used to check if it's equal or not
+        //
+        //<------------------------------JavaScript Variables------------------------>
+        // what are variables?--> container to store data values
+        // single line comment shortcut -> ctrl+/
+        // multi line comment --?/* comment */
+        var Number1= 56;
+        var Number2= 60;
+        console.log( Number1+Number2);
